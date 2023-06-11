1c175744ba3c7dbcac22f1ca23242a0821929ef9
Manroop <lcs2022030@iiitl.ac.in>
2023-06-11 18:08:47 +0530

    Updates

---
+++ precache-manifest.92f12fd9a2514ed1af4041d5aebc1cb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "de291901e44827234cc0",
+    "url": "/TextUtils/static/css/main.458893bf.chunk.css"
+  },
+  {
+    "revision": "de291901e44827234cc0",
+    "url": "/TextUtils/static/js/main.de291901.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "15c69830fe67a4e92e5161a1454c4bef",
+    "url": "/TextUtils/index.html"
+  }
+];
